daf69f84c506d65ea3e9779fcdba1f89cfc48334
Brian Dutz and Yusuf Qedan <brian@>
2016-10-11 09:30:06 -0700

    committed easy way

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -1,13 +1,11 @@
-var apiKey = require('./../.env').apiKey;
 
-$(document).ready(function() { 
+var Weather = require('./../js/weather.js').weatherModule;
+
+$(document).ready(function() {
+  var currentWeatherObject = new Weather();
   $('#weatherLocation').click(function() {
     var city = $('#location').val();
     $('#location').val("");
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response){
-      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
-    }).fail(function(error) {
-      $('.showWeather').text(error.responseJSON.message);
-    });
+    currentWeatherObject.getWeather(city);
   });
 });
